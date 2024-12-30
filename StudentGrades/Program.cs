357dd5d391553f51784d498baffb1272fef09629
Felix Lange <felixlange1@gmail.com>
2024-12-29 19:08:28 -0800

    student objects created

--- StudentGrades/Program.cs
+++ StudentGrades/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<Student> students = new List<Student>();
+
+        Student student1 = new Student()
+        {
+            Name = "John Doe",
+            ID = 513,
+            Grades = { 4.0, 4.0, 3.0, 3.8, 3.5 }
+        };
+
+        Student student2 = new Student()
+        {
+            Name = "Joe Blow",
+            ID = 514,
+            Grades = { 2.0, 2.8, 3.0, 3.8, 4.0 }
+        };
+
+        Student student3 = new Student()
+        {
+            Name = "Joe Jones", 
+            ID = 515,
+            Grades = { 4.0, 4.0, 3.0, 3.8, 3.0 }
+        };
+
+        Student student4 = new Student()
+        {
+            Name = "Jane Doe",
+            ID = 516,
+            Grades = { 4.5, 4.0, 3.0, 4.0, 4.0 }
+        };
+        
+        students.Add(student1);
+        students.Add(student2);
+        students.Add(student3);
+        students.Add(student4);
     }
 }
