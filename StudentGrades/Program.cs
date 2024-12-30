76828882ca0c37cbd7427dad686f4a924097a911
Felix Lange <felixlange1@gmail.com>
2024-12-29 23:14:43 -0800

    called methods and display info

--- StudentGrades/Program.cs
+++ StudentGrades/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace StudentGrades;
+﻿using System.Security.Cryptography;
+
+namespace StudentGrades;
 
 class Program
 {
@@ -10,33 +12,59 @@ class Program
         {
             Name = "John Doe",
             ID = 513,
-            Grades = { 4.0, 4.0, 3.0, 3.8, 3.5 }
         };
+        students.Add(student1);
+        student1.AddGrade(99.0);
+        student1.AddGrade(100.0, 77.4, 89.8, 94.5);
+        Console.WriteLine(student1.Grades);
 
         Student student2 = new Student()
         {
             Name = "Joe Blow",
             ID = 514,
-            Grades = { 2.0, 2.8, 3.0, 3.8, 4.0 }
+
         };
+        students.Add(student2);
+        student2.AddGrade(90.0);
+        student2.AddGrade(93.8, 78.4, 99.8, 87.3);
 
         Student student3 = new Student()
         {
-            Name = "Joe Jones", 
+            Name = "Joe Jones",
             ID = 515,
-            Grades = { 4.0, 4.0, 3.0, 3.8, 3.0 }
         };
+        students.Add(student3);
+        student3.AddGrade(97.8);
+        student3.AddGrade(98.1, 95.0, 94.3, 100.0);
 
         Student student4 = new Student()
         {
             Name = "Jane Doe",
             ID = 516,
-            Grades = { 4.5, 4.0, 3.0, 4.0, 4.0 }
+
         };
-        
-        students.Add(student1);
-        students.Add(student2);
-        students.Add(student3);
         students.Add(student4);
+        student4.AddGrade(100.0);
+        student4.AddGrade(100.0, 99.4, 98.7, 97.0);
+
+        foreach (var student in students)
+        {
+            Console.WriteLine($"Student ID: {student.ID}, Name: {student.Name}");
+            Console.WriteLine("Grades:");
+            foreach (var grade in student.Grades)
+            {
+                Console.WriteLine(grade);
+            }
+
+            Console.WriteLine($"Grade Average: {student.CalculateAverageGrade().ToString("F2")}");
+            Console.WriteLine("---------------------");
+//             Alternative option without nested loops:            
+//            Console.WriteLine($"Student ID: {student.ID}, Name: {student.Name}, Grades: {string.Join(", ", student.Grades.Select(grade => grade.ToString()))}, Grade Average: {student.CalculateAverageGrade()}");};
+            
+        }
+
     }
 }
+
+
+    
